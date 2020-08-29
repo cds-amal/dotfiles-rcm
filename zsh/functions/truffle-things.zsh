c90d9f96138cba58e1be73e13c822f1cb4b8c8b5
Amal Sudama <cds.sudama@gmail.com>
2020-08-28 20:54:03 -0400

    add utility tbg functions

---
+++ zsh/functions/truffle-things.zsh
@@ -0,0 +1,7 @@
+use-truffle-core () {
+    alias truffle=~/work/truffle/packages/core/cli.js
+}
+
+use-truffle-bundle () {
+    alias truffle=node\ ~/work/truffle/packages/truffle/build/cli.bundled.js
+}
