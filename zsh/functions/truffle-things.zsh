4195632634ac4bce64e4b00b66196c55d11ef0a0
Amal Sudama <cds.sudama@gmail.com>
2020-12-28 08:13:25 -0500

    add use-truffle-core-debug

--- zsh/functions/truffle-things.zsh
+++ zsh/functions/truffle-things.zsh
@@ -2,6 +2,10 @@ use-truffle-core () {
     alias truffle=~/work/truffle/packages/core/cli.js
 }
 
+use-truffle-core-debug () {
+    alias truffle=node\ --inspect-brk\ ~/work/truffle/packages/core/cli.js
+}
+
 use-truffle-bundle () {
     alias truffle=node\ ~/work/truffle/packages/truffle/build/cli.bundled.js
 }
